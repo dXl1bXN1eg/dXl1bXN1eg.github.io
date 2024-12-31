1ca4bae0f1ad211c646526e0ee6e594a1deb0d87
cyilmaz <cydepo@gmail.com>
2024-12-31 03:26:49 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,23 @@
 const container = document.getElementById('matrix');
 
-            function createFlyingText() {
-                const textElement = document.createElement('div');
-                textElement.classList.add('flying-text');
-                textElement.textContent = Math.random() > 0.5 ? '0' : '1'; // Rastgele 0 veya 1
+function createFlyingText() {
+ const textElement = document.createElement('div');
+ textElement.classList.add('flying-text');
+ textElement.textContent = Math.random() > 0.5 ? '0' : '1'; // Rastgele 0 veya 1
 
-                // Başlangıç pozisyonu
-                textElement.style.left = Math.random() * 100 + 'vw'; // Ekranın genişliği
+ // Başlangıç pozisyonu
+ textElement.style.left = Math.random() * 100 + 'vw'; // Ekranın genişliği
 
-                container.appendChild(textElement);
+ container.appendChild(textElement);
+ // Animasyonu başlat
+ const duration = Math.random() * 3 + 2; // 2-5 saniye arası
+ textElement.style.animationDuration = duration + 's';
 
-                // Animasyonu başlat
-                const duration = Math.random() * 3 + 2; // 2-5 saniye arası
-                textElement.style.animationDuration = duration + 's';
+ // Animasyon bittiğinde elemanı kaldır
+ setTimeout(() => {
+ textElement.remove();
+ }, duration * 1000);
+ }
 
-                // Animasyon bittiğinde elemanı kaldır
-                setTimeout(() => {
-                    textElement.remove();
-                }, duration * 1000);
-            }
-
-            // Yeni yazı oluşturma süresi
-            setInterval(createFlyingText, 500); // Her 500 ms'de bir yeni yazı oluştur
+ // Yeni yazı oluşturma süresi
+setInterval(createFlyingText, 500); // Her 500 ms'de bir yeni yazı oluştur
